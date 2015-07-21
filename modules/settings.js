b1c5d2d33230b6bc3de8a0e8c4853ccb0bfa23ba
Ammar Hadzic <ammar@Ammars-MacBook-Pro.local>
2015-07-21 14:53:46 +0200

    .sleep added

--- modules/settings.js
+++ modules/settings.js
@@ -4,10 +4,13 @@ module.exports = {
     return browser
       .elementById("org.mightyfrog.android.simplenotepad:id/settings_menu")
         .click()
+        .sleep(1000)
       .elementByXPath("//android.view.View[1]/android.widget.FrameLayout[2]/android.widget.LinearLayout[1]/android.widget.LinearLayout[1]/android.widget.LinearLayout[1]/android.widget.ListView[1]/android.widget.LinearLayout[8]/android.widget.RelativeLayout[1]")
         .click()
+        .sleep(1000)
       .elementByXPath("//android.view.View[1]/android.widget.FrameLayout[2]/android.widget.LinearLayout[1]/android.widget.ListView[1]/android.widget.LinearLayout[3]")
         .click()
+        .sleep(1000)
       .back()
       .back()
   }
